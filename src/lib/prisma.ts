9680f9c05c3149508a4aec3e370a023e23ca2b1c
Ultron09 <connect.singha@gmail.com>
2026-06-18 16:23:26 +0530

    w

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,9 @@
 import { PrismaClient } from '@prisma/client/index.js'
 
 const prismaClientSingleton = () => {
-  return new PrismaClient()
+  return new PrismaClient({
+    datasourceUrl: process.env.DATABASE_URL,
+  })
 }
 
 declare global {
